da17c9a51ec0f02b5303c5977ee6c5d755b7581d
Erin Weston <westonerin1@gmail.com>
2021-01-03 12:53:07 -0500

    add weight differences to weight index

--- src/views/WeightsIndex.vue
+++ src/views/WeightsIndex.vue
@@ -2,8 +2,10 @@
   <div class="weights-index">
     <h1>{{ message }}</h1>
     <div v-for="weight in weights">
-      <h2>{{weight.current_weight}}</h2>
-      <h2>{{weight.date}}</h2></div>
+      <p>{{weight.current_weight}}</p>
+      <p>{{weight.date}}</p>
+      <p>weight difference from yesterday:{{weight.weight_difference}}</p>
+      <p>weight gain/loss over the past week:{{weight.weekly_weight_difference}}</p></div>
   </div>
 </template>
 
